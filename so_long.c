0533eced395fac03c2c2386d36c64ca9cb50a6ea
Abdelkhalek Misk <abmisk@e1r5p2.1337.ma>
2023-07-25 10:34:51 +0100

    leo

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@ int main(int ac, char **av)
 	{
 		if (!ft_strcmp(ft_strnstr(av[1], ".ber", ft_strlen(av[1])), ".ber"))
 			ft_error();
-		printf("OK");
+		
 	}
 	return (0);
 }
---
+++ so_long_parse_map.c
@@ -0,0 +1,13 @@
+#include "so_long.h"
+
+int ft_check_map(char *line, char **av)
+{
+	int i;
+
+	i = 0;
+	line = get_next_line(av[1]);
+	while (line)
+	{
+		
+	}
+}
